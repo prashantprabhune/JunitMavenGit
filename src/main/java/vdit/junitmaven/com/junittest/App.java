a28b204ea23e1a431f88b873c1f2972e0da7257f
pprabhune <pprabhune@PPrabhune-LPT.VDRIVIT.COM>
2018-04-05 15:43:04 +0530

    Test first check-in

---
+++ src/main/java/vdit/junitmaven/com/junittest/App.java
@@ -0,0 +1,13 @@
+package vdit.junitmaven.com.junittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
